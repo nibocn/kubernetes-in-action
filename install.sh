998a2b10545859f00999de8652af68dbe306fc20
NiBo <nibocn@gmail.com>
2019-08-20 11:23:12 +0800

    chore: Initialize project

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+yum makecache fast
+yum update -y
+yum install -y vim wget
+
+# Install Docker
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum makecache fast
+yum install -y docker-ce docker-ce-cli containerd.io
+systemctl start docker
+systemctl enable docker
+
+# Set environment
+cp /vagrant/vimrc /root/.vimrc
+cp /vagrant/vimrc /home/vagrant/.vimrc
